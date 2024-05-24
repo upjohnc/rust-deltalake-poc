5b691d42bb0fbce0bca0b5427149309176631510
Chad Upjohn <chad.upjohn@pinnsg.com>
2024-05-24 12:18:04 -0500

    Using write on deltaops for write mode

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 use deltalake::arrow::datatypes::{DataType as ArrowDataType, Field, Schema};
 use deltalake::kernel::{DataType, PrimitiveType, StructField};
+use deltalake::operations::write::SchemaMode;
 use deltalake::operations::DeltaOps;
-use deltalake::parquet::{
-    basic::{Compression, ZstdLevel},
-    file::properties::WriterProperties,
-};
-use deltalake::writer::{DeltaWriter, RecordBatchWriter};
 use deltalake::{
     arrow::array::RecordBatch,
     datafusion::{
@@ -36,20 +32,15 @@ impl CustomerAmount {
             StructField::new("id", DataType::Primitive(PrimitiveType::Integer), false),
             StructField::new("name", DataType::Primitive(PrimitiveType::String), false),
             StructField::new("amount", DataType::Primitive(PrimitiveType::Integer), false),
-            StructField::new(
-                "half_amount",
-                DataType::Primitive(PrimitiveType::Integer),
-                false,
-            ),
         ]
     }
 }
 
 async fn retriev_source_data(file_parquet: &str) -> Vec<RecordBatch> {
     let context = SessionContext::new();
-    let field_id = Field::new("id", ArrowDataType::Int32, false);
-    let field_name = Field::new("name", ArrowDataType::Utf8, false);
-    let field_amount = Field::new("amount", ArrowDataType::Int32, false);
+    let field_id = Field::new("id", ArrowDataType::Int32, true);
+    let field_name = Field::new("name", ArrowDataType::Utf8, true);
+    let field_amount = Field::new("amount", ArrowDataType::Int32, true);
 
     let schema = Schema::new(vec![field_id, field_name, field_amount]);
 
@@ -87,6 +78,7 @@ async fn get_table(table_path: &str) -> DeltaTable {
 
 #[tokio::main]
 async fn main() {
+    simple_logger::init_with_level(log::Level::Info).unwrap();
     let args: Vec<String> = env::args().collect();
     let source_number: i32 = match args.len() {
         1 => 1,
@@ -96,23 +88,13 @@ async fn main() {
     let file_parquet = format!("source_data/feed_{}.parquet", source_number);
     let table_path = "delta/source_table_bronze";
 
-    let mut table = get_table(&table_path).await;
+    let table = get_table(&table_path).await;
 
-    let writer_properties = WriterProperties::builder()
-        .set_compression(Compression::ZSTD(ZstdLevel::try_new(3).unwrap()))
-        .build();
-
-    let mut writer = RecordBatchWriter::for_table(&table)
-        .expect("Failed to make RecordBatchWriter")
-        .with_writer_properties(writer_properties);
-
-    for batch in retriev_source_data(&file_parquet).await {
-        writer.write(batch).await.unwrap();
-    }
-
-    let adds = writer
-        .flush_and_commit(&mut table)
+    let table_write = DeltaOps(table)
+        .write(retriev_source_data(&file_parquet).await)
+        .with_schema_mode(SchemaMode::Merge)
         .await
-        .expect("Failed to flush write");
-    info!("{} adds written", adds);
+        .unwrap();
+
+    info!("Table Save: {}", table_write);
 }
